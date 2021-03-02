c3b6134398ad3247114dfced23fc2ae3871e80b4
Stephanie Murphy <smurphy@mas.local>
2021-03-02 13:29:17 -0500

    addManager & askQuestion functions

---
+++ index.js
@@ -0,0 +1,58 @@
+const Manager = require("./lib/Manager")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const fs = require("fs")
+const inquirer = require("inquirer")
+const path = require("path")
+const team = []
+
+addManager()
+function addManager() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "managerName",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "What is the manager's Id?"
+        },
+        {
+            type: "input",
+            name: "managerEmail",
+            message: "What is the manager's Email?"
+        },
+        {
+            type: "input",
+            name: "managerOfficeNum",
+            message: "What is the manager's office number?"
+        },
+    ]).then(data => {
+        const manager = new Manager(data.managerName, data.managerId, data.managerEmail, data.managerOfficeNum)
+        team.push(manager)
+        console.log(team)
+        // call the ask question function
+        askQuestion()
+    })
+}
+
+function askQuestion() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "Which type of employee would you like to add?",
+            choices: ["Engineer", "Intern", "N/A"]
+        },
+    ]).then(data => {
+        if(data.choice === "Engineer") {
+            addEngineer()
+        }else if(data.choice === "Intern") {
+            addIntern()
+        }else{
+            buildHtml()
+        }
+    })
+}
